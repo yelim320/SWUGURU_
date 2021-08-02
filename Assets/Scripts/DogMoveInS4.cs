8b4da6d9d2ef12968c88a0ca3c852539c8c67deb
yelim320 <yelimpark320@gmail.com>
2021-08-02 22:22:11 +0900

    스테이지4 구루

---
+++ Assets/Scripts/DogMoveInS4.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMoveInS4 : MonoBehaviour
+{
+    public float t = 1f; // speed
+    public float l = 10f; // 왕복거리
+    public float posX = -18f; // 중심
+
+    public GameObject player;
+    new Collider2D collider2D;
+    float distanceToPlayer;
+    bool isTouching = false;
+
+    private AudioSource bowWow;
+
+    public GameObject doghouse;
+    public enum DogState
+    {
+        Idle,
+        Move,
+        GetSnack
+    }
+
+    public static DogState dogState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dogState = DogState.Move;
+        bowWow = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (dogState)
+        {
+            case DogState.Idle:
+                Idle();
+                break;
+
+            case DogState.Move:
+                Move();
+                break;
+
+            case DogState.GetSnack:
+                GetSnack();
+                break;
+        }
+    }
+
+    public void Idle()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (isTouching)
+        {
+            if (distanceToPlayer <= 5)
+            {
+                return;
+            }
+
+            OnTriggerEnter2D(collider2D);
+            return;
+        }
+    }
+
+    public void Move()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (isTouching)
+        {
+            if (distanceToPlayer <= 5)
+            {
+                return;
+            }
+
+            //Debug.Log("충돌감지");
+
+            OnTriggerEnter2D(collider2D);
+            return;
+        }
+
+        Vector3 pos = new Vector3(posX + Mathf.PingPong(t * Time.time, l), 3.5f, -1);
+        transform.position = pos;
+    }
+
+    public void GetSnack()
+    {
+        float disToDoghouse = Vector3.Distance(transform.position, doghouse.transform.position);
+
+        if (disToDoghouse > 0.1)
+        {
+            Vector3 dirToDoghouse = doghouse.transform.position - transform.position;
+            dirToDoghouse.Normalize();
+            transform.position += dirToDoghouse * 5 * Time.deltaTime;
+        }
+        else
+        {
+            transform.position = doghouse.transform.position;
+            return;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        isTouching = true;
+        if (distanceToPlayer > 5)
+        {
+            //Debug.Log("벗어남");
+            isTouching = false;
+            return;
+        }
+        StartCoroutine(Bark());
+    }
+
+    IEnumerator Bark()
+    {
+        bowWow.Play();
+        yield return new WaitForSeconds(1.0f);
+    }
+}
