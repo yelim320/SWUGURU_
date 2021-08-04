846eae4e610135269c06e442207b88bc82526e0b
yelim320 <yelimpark320@gmail.com>
2021-08-04 22:36:28 +0900

    잡다한 것 수정

--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -7,6 +7,8 @@ public class EndingManager : MonoBehaviour
 {
     public void GameRestart()
     {
+        GageManager.gage = 0;
+        GageManager.animator.SetTrigger("GameOver");
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(1);
     }
